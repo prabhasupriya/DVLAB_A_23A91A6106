5f0929ee54b4cc3d3371223c477a78bb8d2dab04
prabhasupriya <prabhasupriya.b@gmail.com>
2026-03-05 11:31:30 +0530

    week 7 code

---
+++ scripts/WEEK_7.R
@@ -0,0 +1,15 @@
+#data load
+data(HairEyeColor)
+#check structure
+str(HairEyeColor)
+View(HairEyeColor)
+?HairEyeColor
+#very basic mosaic plot(base R)
+mosaicplot(HairEYeColor)
+# add titiles and colors
+
+mosaicplot(HairEyeColor , main="Mosaic Plot: Hair,Eye Color and Sex" , color =TRUE)
+
+
+
+
